082c581d10bd32b9f5c8d70cb18d46d20531101f
Sheepla <62412884+sheepla@users.noreply.github.com>
2021-07-27 16:50:02 +0900

    [update] pkglist.sh

--- pkglist.sh
+++ pkglist.sh
@@ -1,5 +1,12 @@
-# Show the names, versions and descriptions from the PKGBUILD file under the current directory
+# plglist.sh
+#   Show the names, versions and descriptions from the PKGBUILD file under the current directory
+#
+cat << EOS
+| name | version | description |
+|------|---------|-------------|
+EOS
+
 find . -name PKGBUILD | 
     xargs -ro cat | 
-    awk -F '=' -v 'OFS="\t"' '/^pkgname=/{name=$2}; /pkgver=/{ver=$2} /pkgdesc=.*/{print name, ver, $2}' | 
+    awk -F '=' '/^pkgname=/{name=$2}; /pkgver=/{ver=$2} /pkgdesc=.*/{print "|", name, "|", ver, "|", $2, "|"}' | 
     tr -d '"'
