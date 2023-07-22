ef167cb0655f88db5457bd927b9623f9ee93ebdd
sheepla <62412884+sheepla@users.noreply.github.com>
2023-07-22 12:48:01 +0900

    Fix clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . -name '*.zst'  -o -name '*.tar.*' -o -name '*.zip' -type f | xargs -I{} -- rm -v {}
+find . -name '*.zst'  -o -name '*.tar.*' -o -name '*.zip' -o -name 'README*' -o -name 'LICENSE*' -type f | xargs -I{} -- rm -v {}
 find . -name 'pkg' -o -name 'src' -type d | xargs -I{} -- rm -rv {}
